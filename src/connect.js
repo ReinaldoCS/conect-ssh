72df05af362a9f280c262e58c5dc8700935e365f
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2021-08-10 08:55:45 -0300

    update code

--- src/connect.js
+++ src/connect.js
@@ -1,27 +1,27 @@
 var SSH = require('simple-ssh');
 
 var ssh = new SSH({
-    host: '192.168.1.13',
-    user: 'usertest',
-    pass: '1234'
+  host: '192.168.1.13',
+  user: 'usertest',
+  pass: '1234'
 });
 
 
 ssh
-  .exec('pasdwd -v', {
-      out: function(stdout) {
-          console.log(stdout);
-      },
-      exit: function(code) {
-        code && console.log(`Code error: ${code}`);
-    }
-  })
-  .exec('echo $USER', {
-    out: function(stdout) {
-        console.log(stdout);
-    },
-    exit: function(code) {
-      code && console.log(`Code error: ${code}`);
+.exec('cd /home/usertest/test', {
+  out: function(stdout) {
+    console.log(stdout);
+  },
+  exit: function(code) {
+    code && console.log(`Code error: ${code}`);
+  }
+})
+.exec('./script.sh testando codigo com javascript', {
+  out: function(stdout) {
+    console.log(stdout);
+  },
+  exit: function(code) {
+    code && console.log(`Code error: ${code}`);
   }
 })
 
