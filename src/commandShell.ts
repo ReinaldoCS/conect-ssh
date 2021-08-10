b339984ec1ecbe50d49c839a2cea795982f778a8
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2021-08-10 15:16:38 -0300

    fix code

--- src/commandShell.ts
+++ src/commandShell.ts
@@ -1,8 +1,9 @@
+// Variaveis com informações do ciclo recebida pelo jenkins
 const cycle = process.env.CICLO_BILLING;
 const month = process.env.MES_BILLING;
 const year = process.env.ANO_BILLING;
 
-
+// Comando a serem executados no servidor;
 export const commandShell = `
 ls -la;
 pwd;
