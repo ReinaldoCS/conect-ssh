f10cd9ce1f996a5b64dff06e4e7177dd9ae1756b
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2021-10-21 09:54:35 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,34 @@
-import { Client } from 'ssh2';
+// import { Client } from 'ssh2';
 
-import { commandShell } from './commandShell'
+// import { commandShell } from './commandShell'
 
-const conn = new Client();
+// const conn = new Client();
 
-conn.on('ready', () => {
-  console.log('Client :: ready');
-  conn.shell((err, stream) => {
-    if (err) throw err;
-    stream.on('close', () => {
-      console.log('Stream :: close');
-      conn.end();
-    }).on('data', (data: string) => {
-      console.log('OUTPUT: ' + data);
-    });
-    stream.end(commandShell);
-  });
-}).connect({
-host: process.env.HOST_SERVER,
-port: 22,
-username: process.env.HOST_SERVER_USER,
-password: process.env.HOST_SERVER_PASS
-});
+// conn.on('ready', () => {
+//   console.log('Client :: ready');
+//   conn.shell((err, stream) => {
+//     if (err) throw err;
+//     stream.on('close', () => {
+//       console.log('Stream :: close');
+//       conn.end();
+//     }).on('data', (data: string) => {
+//       console.log('OUTPUT: ' + data);
+//     });
+//     stream.end(commandShell);
+//   });
+// }).connect({
+// host: process.env.HOST_SERVER,
+// port: 22,
+// username: process.env.HOST_SERVER_USER,
+// password: process.env.HOST_SERVER_PASS
+// });
 
 
+console.log("==========================================");
+console.log("JENKINS RODOU O CÓDIGO");
+console.log("==========================================");
+console.log(process.env.TEST_VAR);
+console.log("==========================================");
 // host: '192.168.100.125',
 // port: 22,
 // username: 'usertest',
